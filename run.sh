daca5d1cc7caefefa1580432368ee3a2e09bf24f
Guy Arbitman <guy20495@gmail.com>
2021-10-28 15:47:35 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k ACCESS_KEY)
+SECRET_ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k SECRET_ACCESS_KEY)
+STORAGE_PROVIDER=$(/opt/elasticbeanstalk/bin/get-config environment -k STORAGE_PROVIDER)
+BUCKET_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k BUCKET_NAME)
+FOLDER_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k FOLDER_NAME)
+
+sudo systemctl restart docker
+sudo docker run -d --rm --net host -e ACCESS_KEY="${ACCESS_KEY}" -e SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY}" \
+  -e FOLDER_NAME="${FOLDER_NAME}" -e BUCKET_NAME="${BUCKET_NAME}" -e STORAGE_PROVIDER="${STORAGE_PROVIDER}" --log-driver json-file --log-opt max-size=10m --log-opt max-file=5 seekret/sniffer:2
